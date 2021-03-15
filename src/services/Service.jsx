eed80b28f6387cb09cb886e928eb2352ac08cbdb
d.kozlov <d.kozlov@hottelecom.by>
2021-03-15 21:03:19 +0300

    add gh pages

--- src/services/Service.jsx
+++ src/services/Service.jsx
@@ -1,19 +1,24 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Service {
+
+    _apiBase = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+
     async getMovie(movie) {
-        const res = await fetch(`http://www.omdbapi.com/?apikey=1aa3e346&s=${movie}`);
+        const res = await fetch(`${this._apiBase}&s=${movie}`);
 
         if (!res.ok) {
-            throw new Error(`Could not fetch ${`http://www.omdbapi.com/?apikey=1aa3e346&s=${movie}`}, received ${res.status}`);
+            throw new Error(`Could not fetch ${`${this._apiBase}&s=${movie}`}, received ${res.status}`);
         }
 
         return await res.json();
     }
 
     async getMovieByType(movie, type) {
-        const res = await fetch(`http://www.omdbapi.com/?apikey=1aa3e346&s=${movie}&type=${type}`);
+        const res = await fetch(`${this._apiBase}&s=${movie}&type=${type}`);
 
         if (!res.ok) {
-            throw new Error(`Could not fetch ${`http://www.omdbapi.com/?apikey=1aa3e346&s=${movie}&type=${type}`}, received ${res.status}`);
+            throw new Error(`Could not fetch ${`${this._apiBase}&s=${movie}&type=${type}`}, received ${res.status}`);
         }
 
         return await res.json();
